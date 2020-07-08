8d62615892df85ef91c9e355e95fcdc94dba41d2
Dang Pham <thienvu453@gmail.com>
2020-07-08 09:25:38 +0700

    project init

---
+++ src/main/java/com/codegym/simplewebspringboot/SimpleWebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.simplewebspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/simplewebspringboot/SimpleWebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.simplewebspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
